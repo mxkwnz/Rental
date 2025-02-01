5b7542856f2800b0369fbf1e37d737b7b146871e
Ekosik7 <zzh.eraly121206@gmail.com>
2025-02-01 12:27:51 +0500

    Update Main.java

--- src/com/carrental/main/Main.java
+++ src/com/carrental/main/Main.java
@@ -1,32 +1,25 @@
 package com.carrental.main;
 
-import com.carrental.services.*;
-import com.carrental.interfaces.*;
-import java.sql.Date;
+import com.carrental.controller.CarController;
+import com.carrental.controller.UserController;
+import com.carrental.controller.RentalController;
+import com.carrental.services.CarService;
+import com.carrental.services.UserService;
+import com.carrental.services.RentalService;
+
 import java.sql.SQLException;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Scanner scanner = new Scanner(System.in);
-        ICarService carService = new CarService();
-        IUserService userService = new UserService();
-        IRentalService rentalService = new RentalService();
+        CarService carService = new CarService();
+        UserService userService = new UserService();
+        RentalService rentalService = new RentalService();
 
-        carService.showAvailableCars();
+        CarController carController = new CarController(carService);
+        UserController userController = new UserController(userService);
+        RentalController rentalController = new RentalController(rentalService, userService, carService);
 
-        System.out.print("Введите ID автомобиля для аренды: ");
-        int carId = scanner.nextInt();
-        System.out.print("Введите ID пользователя (или 0 для создания нового пользователя): ");
-        int userId = scanner.nextInt();
-        System.out.print("Введите дату начала аренды (YYYY-MM-DD): ");
-        String startDateStr = scanner.next();
-        System.out.print("Введите дату окончания аренды (YYYY-MM-DD): ");
-        String endDateStr = scanner.next();
-
-        Date startDate = Date.valueOf(startDateStr);
-        Date endDate = Date.valueOf(endDateStr);
-
-        rentalService.rentCar(carId, userId, startDate, endDate);
+        MyAppApplication app = new MyAppApplication(carController, userController, rentalController);
+        app.run();
     }
 }
