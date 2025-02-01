9e66c926afbe4d01f4c28da70e89364aa1d5e3c3
Ekosik7 <zzh.eraly121206@gmail.com>
2025-02-01 12:28:18 +0500

    Update ICarService.java

--- src/com/carrental/interfaces/ICarService.java
+++ src/com/carrental/interfaces/ICarService.java
@@ -2,11 +2,13 @@ package com.carrental.interfaces;
 
 import com.carrental.models.Car;
 
-import java.sql.Date;
 import java.sql.SQLException;
+import java.util.List;
 
 public interface ICarService {
-    void showAvailableCars() throws SQLException;
-
+    List<Car> getAvailableCars() throws SQLException;
+    void addNewCar(String name, String model, double pricePerDay) throws SQLException;
+    void removeCar(int carId) throws SQLException;
     Car getCarById(int carId) throws SQLException;
+    boolean isCarAvailable(int carId) throws SQLException;
 }
