5628611f9a5960281082df5bc2b438bc29c1e4a5
Ekosik7 <zzh.eraly121206@gmail.com>
2025-02-01 12:29:05 +0500

    Update IRentalService.java

--- src/com/carrental/interfaces/IRentalService.java
+++ src/com/carrental/interfaces/IRentalService.java
@@ -1,8 +1,12 @@
 package com.carrental.interfaces;
 
-import java.sql.Date;
 import java.sql.SQLException;
 
 public interface IRentalService {
-    void rentCar(int carId, int userId, Date startDate, Date endDate) throws SQLException;
+    void rentCar(int carId, int userId, String startDate, String endDate) throws SQLException;
+    void removeRental(int rentalId) throws SQLException;
+    void printRentalInvoice(int rentalId) throws SQLException;
+    boolean isCarOccupied(int carId, String startDate, String endDate) throws SQLException;
+    void removeRentalDaysByPeriod(int rentalId, String startDate, String endDate) throws SQLException;  // Добавленный метод
+    void updateRental(int rentalId, String newStartDate, String newEndDate) throws SQLException;
 }
