10c8024b0d2f4d8cd80b07086198b938c2c013fe
Ekosik7 <zzh.eraly121206@gmail.com>
2025-02-01 12:28:50 +0500

    Update IUserService.java

--- src/com/carrental/interfaces/IUserService.java
+++ src/com/carrental/interfaces/IUserService.java
@@ -1,8 +1,13 @@
 package com.carrental.interfaces;
 
+import com.carrental.models.User;
+
 import java.sql.SQLException;
+import java.util.List;
 
 public interface IUserService {
+    List<User> getAllUsers() throws SQLException;
     boolean userExists(int userId) throws SQLException;
-    void addNewUser(int userId) throws SQLException;
+    void addNewUser(String name, String email, String phone) throws SQLException;
+    void removeUser(int userId) throws SQLException;
 }
