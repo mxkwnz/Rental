14dd2bc627cac3108dd2678ab80f151f9a47651e
Ekosik7 <zzh.eraly121206@gmail.com>
2025-02-14 20:02:08 +0500

    FINAL )

--- src/com/carrental/utils/Validator.java
+++ src/com/carrental/utils/Validator.java
@@ -1,11 +1,12 @@
 package com.carrental.utils;
 public class Validator {
+
     public static boolean isValidEmail(String email) {
         return email.matches("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$");
     }
 
-
     public static boolean isValidPrice(double price) {
         return price > 0;
     }
+
 }
